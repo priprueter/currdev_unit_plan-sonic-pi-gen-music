d425b0bc34f505c08f92995ecec53679047b2efd
Liam Baum <liamcbaum@gmail.com>
2021-12-19 21:43:26 -0500

    updated file titles

--- code/Lesson_5_Single_Line_Conditional_Observation_Example.rb
+++
@@ -1,9 +0,0 @@
-# Single Line Conditional Observation Example
-
-live_loop :conditional do
-  use_random_seed Time.now.to_i
-  puts "flip!"
-  play 60 if rrand_i == 0
-  play 80 if rrand_i == 1
-  sleep 1
-end
